fbd18f454cced486c0cc353c49d75a74d22fab67
Marco Antonio Arantes <marcoantoniopou@gmail.com>
2024-09-04 10:50:20 -0300

    feat: Arrumando estrutura dos arquivos

---
+++ src/pages/Consulta/Consulta.tsx
@@ -0,0 +1,19 @@
+import BotaoCTA from "../../components/BotaoCTA/BotaoCTA";
+import "../../global.scss"
+import "./Consulta.scss"
+
+function Consulta() {
+    
+  return (
+    <>
+      <div className="teste margem_10">
+        <p>Olá, tudo bem?</p>
+        <div>
+          <BotaoCTA escrito="teste" aparencia="primario" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Consulta;
