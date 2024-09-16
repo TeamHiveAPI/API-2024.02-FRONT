c2ff908690786cabce50653fa8deb2e16759f792
Júlia Rosado <142221388+juliamariahr@users.noreply.github.com>
2024-09-16 08:06:09 -0300

    feat: Conexão back e front da página de Login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,33 +1,61 @@
 import { useState } from "react";
+import axios, { AxiosError, AxiosResponse } from "axios"; 
 import BotaoCTA from "../../components/BotaoCTA/BotaoCTA";
 import Navbar from "../../components/Navbar/Navbar";
 import FormInputSenha from "../../components/FormInput/FormInputSenha";
 import FormInputEmail from "../../components/FormInput/FormInputEmail";
-import "./Login.scss"
-
+import "./Login.scss";
 
 function Login() {
-    
-    const [nome, setNome] = useState("");
-    const [senha, setSenha] = useState("");
- 
-    return (
-    <>
-      <Navbar />
+    const [email, setEmail] = useState("");  // Estado para armazenar o email
+    const [senha, setSenha] = useState("");  // Estado para armazenar a senha
 
-        <div className="login_container">
-            <div className="margem_10 login_box_container">
-                <h2>LOGIN</h2>
-                <FormInputEmail onEmailChange={setNome} />
-                <FormInputSenha type="normal" onSenhaChange={setSenha} />
-                <div className="login_baixo">
-                    <BotaoCTA escrito="Entrar" aparencia="primario" />
-                    <BotaoCTA link="/cadastro" escrito="Cadastre-se" aparencia="secundario" />
+    // Função para realizar o login
+    const handleLogin = () => {
+        // Dados que serão enviados para o backend
+        const dadosLogin = {
+            email: email, 
+            senha: senha
+        };
+
+        // Enviando a requisição POST para o backend
+        axios.post('http://localhost:8080/login', dadosLogin)
+            .then((response: AxiosResponse) => {  // Tipagem explícita para 'response'
+                // Se o login for bem-sucedido
+                alert("Login realizado com sucesso!");
+                console.log(response.data);
+            })
+            .catch((error: AxiosError) => {  // Tipagem explícita para 'error'
+                // Se houver um erro no login
+                alert("Erro ao realizar o login. Verifique suas credenciais.");
+                console.error(error);
+            });
+    };
+
+    return (
+        <>
+            <Navbar />
+
+            <div className="login_container">
+                <div className="margem_10 login_box_container">
+                    <h2>LOGIN</h2>
+                    {/* Input de email */}
+                    <FormInputEmail onEmailChange={setEmail} />
+
+                    {/* Input de senha */}
+                    <FormInputSenha type="normal" onSenhaChange={setSenha} />
+
+                    <div className="login_baixo">
+                        {/* Botão para enviar o login */}
+                        <BotaoCTA escrito="Entrar" aparencia="primario" onClick={handleLogin} />
+
+                        {/* Botão para redirecionar ao cadastro */}
+                        <BotaoCTA link="/cadastro" escrito="Cadastre-se" aparencia="secundario" />
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default Login;
