867763eddda9bcb9afcb6fe8ad4bc143163ba3f1
Marco Antonio Arantes <marcoantoniopou@gmail.com>
2024-10-15 18:11:30 -0300

    refactor: Trocando seção de explicação por botões de filtro na pág. Consulta

--- src/pages/Consulta/Consulta.tsx
+++ src/pages/Consulta/Consulta.tsx
@@ -17,7 +17,14 @@ interface Projeto {
 }
 
 function Consulta() {
+
   const navigate = useNavigate();
+
+  const [ativo, setAtivo] = useState('Tudo'); // Inicialmente "Tudo" está ativo
+
+  const handleBotaoPesquisa = (nome: any) => {
+    setAtivo(nome); // Define o botão clicado como ativo
+  };
 
   const [termoPesquisa, setTermoPesquisa] = useState(""); // Armazena o termo de pesquisa
   const [projetos, setProjetos] = useState<Projeto[]>([]);
@@ -57,18 +64,17 @@ function Consulta() {
     carregarTabelaConsulta();
   }, [navigate]); //
   
-
   return (
     <>
       <Navbar />
-
+  
       {/* Implementar lógica de mostrar barra depois que ter o JWT*/}
-
+  
       {/* Se for um usuário, mostrar essa barra */}
-      <div className="margem_10 cons_container mtop80">
+      <div className="margem_10 cons_container mtop80 align_center">
         <h1>Consulta de Projetos</h1>
       </div>
-
+  
       {/* Se for um admin, mostrar essa barra */}
       <div className="margem_10 cons_admin">
         <div className="cons_container cons_admin_esq">
@@ -76,87 +82,73 @@ function Consulta() {
         </div>
         <div className="cons_botoes_admin">
           <button className="cons_botao_grande" onClick={() => navigate("/cadastro-projeto")}>
-            <img src="img/criar_projeto.svg" />
+            <img src="img/criar_projeto.svg" alt="Cadastrar Projeto"/>
             <p>Cadastrar Projeto</p>
           </button>
           <button className="cons_botao_grande" onClick={() => navigate("/dashboard")}>
-            <img src="img/dashboard.svg" />
+            <img src="img/dashboard.svg" alt="Dashboard"/>
             <p>Dashboard</p>
           </button>
         </div>
       </div>
-
-      <div className="margem_10 cons_container ">
+  
+      <div className="margem_10 cons_container">
         <div className="cons_barra_pesquisa">
-          <input type="text" placeholder="Pesquisar" value={termoPesquisa}
-            onChange={(e) => setTermoPesquisa(e.target.value)}/>
+          <input
+            type="text"
+            placeholder="Pesquisar"
+            value={termoPesquisa}
+            onChange={(e) => setTermoPesquisa(e.target.value)}
+          />
           <BotaoCTA img="img/pesquisa.svg" escrito="Buscar" aparencia="primario" onClick={handleSearch} />
         </div>
-        <div className="cons_explicacao">
-          <h4>Pesquise por palavras-chave. É possível pesquisar pelos seguintes tópicos:</h4>
-          <div className="cons_topicos">
-          <div className="cons_topicos_esq">
-                <h4>Referência do Projeto</h4>
-                <h4>Coordenador</h4>
-                <div className="cons_topico_detalhe">
-                  <h5>Classificação</h5>
-                  <div>
-                    <p className="cons_detalhe">• Contrato</p>
-                    <p className="cons_detalhe">• Convênio</p>
-                    <p className="cons_detalhe">• Patrocínio</p>
-                    <p className="cons_detalhe">• Termo de Cooperação</p>
-                    <p className="cons_detalhe">• Termo de Outorga</p>
-                  </div>
-                </div>
-              </div>
-              <div className="cons_topicos_dir">
-                <h4>Data de Início</h4>
-                <h4>Data de Término</h4>
-                <div className="cons_topico_detalhe">
-                  <h5>Situação do Projeto</h5>
-                  <div>
-                    <p className="cons_detalhe">• Projetos Não Iniciados</p>
-                    <p className="cons_detalhe">• Projetos em Andamento</p>
-                    <p className="cons_detalhe">• Projetos Encerrados</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="cons_botao_pesquisa_container">
+          <button className={`cons_botao_pesquisa ${ativo === 'Tudo' ? 'ativo' : ''}`}onClick={() => handleBotaoPesquisa('Tudo')}> Tudo </button>
+          <button className={`cons_botao_pesquisa ${ativo === 'Ref. Projeto' ? 'ativo' : ''}`} onClick={() => handleBotaoPesquisa('Ref. Projeto')}> Ref. Projeto </button>
+          <button className={`cons_botao_pesquisa ${ativo === 'Coordenador' ? 'ativo' : ''}`} onClick={() => handleBotaoPesquisa('Coordenador')}> Coordenador </button>
+          <button className={`cons_botao_pesquisa ${ativo === 'Contratante' ? 'ativo' : ''}`} onClick={() => handleBotaoPesquisa('Contratante')}>Contratante </button>
+          <button className={`cons_botao_pesquisa ${ativo === 'Data de Início' ? 'ativo' : ''}`} onClick={() => handleBotaoPesquisa('Data de Início')}> Data de Início </button>
+          <button className={`cons_botao_pesquisa ${ativo === 'Data de Término' ? 'ativo' : ''}`} onClick={() => handleBotaoPesquisa('Data de Término')}> Data de Término </button>
+        </div>
       </div>
-        
-
+  
       <table className="margem_10">
-  <thead>
-    <tr>
-      <th className="th_botao"> </th>
-      <th className="th_referencia">Referência do Projeto</th>
-      <th className="th_data">Início</th>
-      <th className="th_data">Término</th>
-      <th className="th_coordenador">Coordenador</th>
-      <th className="th_valor">Valor</th>
-    </tr>
-  </thead>
-  <tbody>
-    {projetos.length > 0 ? (
-      projetos.map((projeto) => (
-        <tr key={projeto.id}>
-          <td><img src="img/detalhe_arquivo.svg" alt="detalhe" className="detalhe_projeto" onClick={() => navigate(`/projeto/${projeto.id}`)}/></td>
-          <td className="referencia">{projeto.referencia}</td>
-          <td>{formatarData(projeto.dataInicio)}</td>
-          <td>{projeto.dataTermino ? formatarData(projeto.dataTermino) : "Em Processo"}</td>
-          <td className="coordenador">{projeto.coordenador}</td>
-          <td>R${projeto.valor.toFixed(2)}</td>
-        </tr>
-      ))
-    ) : (
-    <tr className="cons_nenhum">
-      <td colSpan={6}>Nenhum projeto encontrado.</td>
-    </tr>
-    )}
-  </tbody>
-</table>
-
+        <thead>
+          <tr>
+            <th className="th_botao"> </th>
+            <th className="th_referencia">Referência do Projeto</th>
+            <th className="th_data">Início</th>
+            <th className="th_data">Término</th>
+            <th className="th_coordenador">Coordenador</th>
+            <th className="th_valor">Valor</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projetos.length > 0 ? (
+            projetos.map((projeto) => (
+              <tr key={projeto.id}>
+                <td>
+                  <img
+                    src="img/detalhe_arquivo.svg"
+                    alt="detalhe"
+                    className="detalhe_projeto"
+                    onClick={() => navigate(`/projeto/${projeto.id}`)}
+                  />
+                </td>
+                <td className="referencia">{projeto.referencia}</td>
+                <td>{formatarData(projeto.dataInicio)}</td>
+                <td>{projeto.dataTermino ? formatarData(projeto.dataTermino) : "Em Processo"}</td>
+                <td className="coordenador">{projeto.coordenador}</td>
+                <td>R${projeto.valor.toFixed(2)}</td>
+              </tr>
+            ))
+          ) : (
+            <tr className="cons_nenhum">
+              <td colSpan={6}>Nenhum projeto encontrado.</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </>
   );
 }
